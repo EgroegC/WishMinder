7ff9c435d793c3370b95777d877a01f3e68dd723
EgroegC <giorgosaleksakis@gmail.com>
2025-03-14 22:20:18 +0200

    Add unit testing

--- backend/src/models/user/user_validation.js
+++ backend/src/models/user/user_validation.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 function validateUser(user) {
     const userSchema = Joi.object({
         name: Joi.string().min(3).max(50).required(),
-        email: Joi.string().min(9).max(255).required().email(),
+        email: Joi.string().min(12).max(255).required().email(),
         password: Joi.string().min(5).max(255).required(),
       });
       
