1796e128ec80f07c9d53099677b6b9861b9ff250
EgroegC <giorgosaleksakis@gmail.com>
2025-03-16 12:37:00 +0200

    refactor so i can see the name of the db

--- backend/src/startup/db.js
+++ backend/src/startup/db.js
@@ -1,7 +1,8 @@
 const pool = require('../config/db')();
 const logger = require('../config/logger')();
+const config = require("config");
 
 module.exports = function(){
     pool.connect()
-    .then(() => logger.info("Connected to the database"));
+    .then(() => logger.info(`Connected to the ${config.get("database.name")} database`))
 }
