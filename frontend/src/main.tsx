a8584eed24bf3b21d6083c0158b90db46d8d631c
EgroegC <giorgosaleksakis@gmail.com>
2025-03-27 10:44:43 +0200

    Install Chakra UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from "@/components/ui/provider"; //chakra
 import App from "./App.tsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Provider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
   </StrictMode>
 );
