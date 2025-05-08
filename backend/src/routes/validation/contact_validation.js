30a804ae5e05d1d170f78f26942c5a1ea6583fcf
EgroegC <giorgosaleksakis@gmail.com>
2025-05-08 08:25:08 +0300

    Change in contact validation to accept birthdays at least one year ago, add proper messages and create unit test

--- backend/src/routes/validation/contact_validation.js
+++ backend/src/routes/validation/contact_validation.js
@@ -1,21 +1,28 @@
 const Joi = require('joi');
 
 function validateContact(contact) {
+    const oneYearAgo = new Date();
+    oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+
     const contactSchema = Joi.object({
         name: Joi.string().min(3).max(50).required(),
         surname: Joi.string().min(3).max(50).required(),
         email: Joi.string().min(12).max(255).required().email(),
         phone: Joi.string()
-        .pattern(/^\+?[1-9]\d{1,14}$/) // Validates international E.164 format
-        .required()
-        .messages({
-            "string.pattern.base": "Phone number must be a valid format (e.g., +123456789 or 1234567890).",
-            "any.required": "Phone number is required."
-        }), 
-        birthdate: Joi.date().iso().max("now").optional(),
+            .pattern(/^\+?[1-9]\d{1,14}$/)
+            .required()
+            .messages({
+                "string.pattern.base": "Phone number must be a valid format (e.g., +123456789 or 1234567890).",
+                "any.required": "Phone number is required."
+            }), 
+        birthdate: Joi.date().iso()
+            .max(oneYearAgo).optional()
+            .messages({'date.base': 'Birthdate must be a valid date.',
+                'date.format': 'Birthdate must be in ISO format (YYYY-MM-DD).',
+                'date.max': `Birthdate must be at least one year before today.`,}),
     });
-      
-  return contactSchema.validate(contact);
+
+    return contactSchema.validate(contact);
 }
 
 module.exports = validateContact;
---
+++ backend/tests/unit/routes/validation/contact_validation.test.js
@@ -0,0 +1,183 @@
+const validateContact = require('../../../../src/routes/validation/contact_validation');
+
+const oneYearAgo = new Date();
+oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+
+describe('validate_contact', () => {
+
+  let name, surname, email, phone, birthdate;
+
+  beforeEach(async () => {
+    name = 'Test_user';
+    surname = 'Test_surname'
+    email = 'test_user@gmail.com';
+    phone = '6987213621'
+    birthdate = oneYearAgo;
+  });
+
+  describe("test a valid contact", () => {
+    const validPhones = ['+1234567890', '1234567890'];
+
+    test.each(validPhones)("should validate contact with phone: %s", (phone) => {
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeUndefined();
+    });
+
+    it("should validate contact without birthdate", () => {
+        const { error } = validateContact({ name, surname, email, phone });
+        expect(error).toBeUndefined();
+    });
+  });
+
+  describe("tets an invalid name", () => {
+    it("should return an error if name is missing", () => {
+
+      const { error } = validateContact({ surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/name.*required/i);
+    });
+
+    it("Should return an error if name is too short", () => {
+      name = 'Te';
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/name.*3 characters/i);
+    });
+
+    it('should return an error if name is too long', () => {
+      name = "J".repeat(51);
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/name.*50 characters/i);
+    });
+  });
+
+  describe("tets an invalid surname", () => {
+    it("should return an error if surname is missing", () => {
+
+      const { error } = validateContact({ name, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/surname.*required/i);
+    });
+
+    it("Should return an error if surname is too short", () => {
+      surname = 'Su';
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/surname.*3 characters/i);
+    });
+
+    it('should return an error if surname is too long', () => {
+      surname = "J".repeat(51);
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/surname.*50 characters/i);
+    });
+  });
+
+  describe('test an invalid email', () => {
+    it("should return an error if email is missing", () => {
+
+      const { error } = validateContact({ name, surname, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/email.*required/i);
+    });
+
+    it("Should return an error if email is too short", () => {
+      email = "e@gmail.com"
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/email.*12 characters/i);
+    });
+
+    it('should return an error if email is too long', () => {
+      email = "J".repeat(256);
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/email.*255 characters/i);
+    });
+
+    it("should return an error if email is invalid", () => {
+      email = 'not-an-email';
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/email.*valid/i);
+    });
+  });
+
+  describe("test an invalid phone", () => {
+    it("should return an error if phone is missing", () => {
+
+      const { error } = validateContact({ name, surname, email, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/phone.*required/i);
+    });
+
+    it("should return an error if phone is too short", () => {
+      phone = '3';
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/phone.* valid/i);
+    });
+
+    it('should return an error if phone is too long', () => {
+      phone = "+1234567890123457";
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/phone.* valid/i);
+    });
+
+    it('should return an error if phone starts with 0', () => {
+        phone = "+0123425";
+  
+        const { error } = validateContact({ name, surname, email, phone, birthdate });
+        expect(error).toBeDefined();
+        expect(error.details[0].message).toMatch(/phone.* valid/i);
+    });
+
+    it('should return an error if phone is non numeric', () => {
+        phone = "+0123bb25";
+  
+        const { error } = validateContact({ name, surname, email, phone, birthdate });
+        expect(error).toBeDefined();
+        expect(error.details[0].message).toMatch(/phone.* valid/i);
+    });
+  });
+
+  describe('test an invalid birthdate', () => {
+    it("Should return an error if birthdate has not a valid format", () => {
+      birthdate = "12/05/2002"
+
+      const { error } = validateContact({ name, surname, email, phone, birthdate });
+      expect(error).toBeDefined();
+      expect(error.details[0].message).toMatch(/birthdate.* format/i);
+    });
+
+    it("Should return an error if birthdate is not a valid Date", () => {
+        birthdate = new Date();
+  
+        const { error } = validateContact({ name, surname, email, phone, birthdate });
+        expect(error).toBeDefined();
+        expect(error.details[0].message).toMatch(/birthdate.* at least one/i);
+    });
+
+    it("Should return an error if birthdate is not a parsable date ", () => {
+        birthdate = 874373278; 
+      
+        const { error } = validateContact({ name, surname, email, phone, birthdate });
+        expect(error).toBeDefined();
+        expect(error.details[0].message).toMatch(/birthdate.* valid/i);
+      });
+  });
+})
+
+
