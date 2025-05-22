ba0d18d067871f5355d0808c1d7e4e7befd7d946
EgroegC <giorgosaleksakis@gmail.com>
2025-05-22 12:27:03 +0300

    Add frontend neltify url in the allowed ones in cors

--- backend/src/startup/cors.js
+++ backend/src/startup/cors.js
@@ -1,8 +1,20 @@
 const cors = require("cors");
 
+const allowedOrigins = [
+  'http://localhost:5173',                      
+  'https://lovely-puffpuff-4a6e3e.netlify.app'     
+];
+
 module.exports = function(app) {
-    app.use(cors({
-        origin: 'http://localhost:5173', 
-        credentials: true 
-    }));
+  app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl, Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS')); 
+      }
+    },
+    credentials: true
+  }));
 }
