6870c471139c68e920df2424950e501c7d481342
EgroegC <giorgosaleksakis@gmail.com>
2025-05-27 15:16:06 +0300

    Add git environment for identifying the secrets. Correct the web push condif file message

--- backend/src/config/web_push.js
+++ backend/src/config/web_push.js
@@ -2,10 +2,10 @@ const webpush = require('web-push');
 
 module.exports = function(){ 
 
-if (!process.env.VAPID_PUBLIC_KEY && process.env.VAPID_PRIVATE_KEY) {
-        throw new Error("FATAL ERROR: DATABASE_PASSWORD is not set in .env file");
+    if (!process.env.VAPID_PUBLIC_KEY && process.env.VAPID_PRIVATE_KEY) {
+            throw new Error("FATAL ERROR: VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY are not set in .env file");
     }
-
+    
     webpush.setVapidDetails(
         'mailto:george@gmail.com', // Doesn't have to be real, but must be a valid email format
         process.env.VAPID_PUBLIC_KEY,
