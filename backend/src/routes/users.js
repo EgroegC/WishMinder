ee64a9d5c4eabe572ada05d6b680e328413b2c7d
EgroegC <giorgosaleksakis@gmail.com>
2025-05-28 23:05:17 +0300

    Change return code for incorrect body. Print message when user already exists.

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -60,7 +60,7 @@ router.get('/me', authenticateToken, async (req, res) => {
  */
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(422).send(error.details[0].message);
   
     let user = await User.findByEmail(req.body.email);
     if (user) return res.status(400).send('User already registered.');
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,8 +1,10 @@
 import AuthForm from "../components/LoginSignUpForm/LoginSignupForm";
 import { useNavigate } from "react-router-dom";
 import apiClient from "../services/api-client";
+import { useState } from "react";
 
 function SignupPage() {
+  const [authError, setAuthError] = useState("");
   const navigate = useNavigate();
 
   function handleSignup(email: string, password: string, name?: string) {
@@ -11,12 +13,18 @@ function SignupPage() {
       .then(() => {
         navigate("/login");
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        if (err.response?.status === 400) {
+          setAuthError("User already registered.");
+        } else {
+          setAuthError("Something went wrong. Please try again.");
+        }
+      });
   }
 
   return (
     <div className="auth-page">
-      <AuthForm isSignup={true} onSubmit={handleSignup} />
+      <AuthForm isSignup={true} onSubmit={handleSignup} authError={authError} />
     </div>
   );
 }
