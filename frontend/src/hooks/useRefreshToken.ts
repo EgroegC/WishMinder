398ed7efffbf3e1ce31d244a78db64dbcbe205b4
EgroegC <giorgosaleksakis@gmail.com>
2025-06-11 01:28:20 +0300

    feat(hook): Create useRefreshToken custom hook for refreshing the jwt

---
+++ frontend/src/hooks/useRefreshToken.ts
@@ -0,0 +1,32 @@
+import apiClient from "../services/api-client";
+import { AxiosError } from 'axios'
+import { useAuth } from "./useAuth";
+
+const useRefreshToken = () => {
+    const { setAccessToken } = useAuth();
+
+    const refresh = async () => {
+        try {
+            const response = await apiClient.post('/api/auth/refresh', {}, { withCredentials: true });
+
+            setAccessToken(response.data.accessToken);
+            return response.data.accessToken;
+        } catch (err) {
+            if (err instanceof AxiosError) {
+                if (isRefreshTokenInvalidOrExpired(err)) {
+                    return null;
+                }
+            }
+
+            return null;
+        }
+    };
+    return refresh;
+};
+
+const isRefreshTokenInvalidOrExpired = (err: AxiosError) => {
+    return err?.response?.status === 403 &&
+        (err?.response?.data === 'Refresh token expired.' || err?.response?.data === 'Invalid refresh token.')
+}
+
+export default useRefreshToken;
