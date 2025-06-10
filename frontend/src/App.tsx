cdd128650267898857599dc5e375ef231971766e
EgroegC <giorgosaleksakis@gmail.com>
2025-06-11 01:21:00 +0300

    chore: install Avatar and theming from  shadcn, customize NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom"
 import AppLayout from "./components/layout/AppLayout"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
+import { ThemeProvider } from "./components/theme-provider"
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<AppLayout><HomePage /></AppLayout>} />
-    </Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<AppLayout><HomePage /></AppLayout>} />
+      </Routes>
+    </ThemeProvider>
   )
 }
 
--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -1,12 +1,29 @@
+import { LogOut, Moon, Sun } from 'lucide-react';
 import { Link } from 'react-router-dom';
-
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from '../ui/button';
+import { useTheme } from '../theme-provider';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { useAuth } from '@/hooks/useAuth';
+import useLogout from '@/hooks/useLogout';
 
 const NavBar = () => {
+    const { setTheme } = useTheme();
+    const { accessToken, setAccessToken } = useAuth();
+    const logout = useLogout();
 
     return (
         <nav className=' p-4 flex items-center justify-between'>
             {/* LEFT SIDE */}
-            sidebarButton
+            <SidebarTrigger />
             {/* CENTER */}
             <div className="flex justify-center items-center w-1/3">
                 <Link
@@ -19,6 +36,60 @@ const NavBar = () => {
             {/* RIGHT SIDE */}
             <div className='flex items-center gap-4' >
                 {/* THEME MENU */}
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                            <span className="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => setTheme("light")}>
+                            Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                            Dark
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("system")}>
+                            System
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                {/* USER MENU */}
+                {accessToken ? (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent sideOffset={10}>
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem
+                                onClick={async () => {
+                                    await logout();
+                                    setAccessToken(null);
+                                }}
+                                className="text-destructive focus:text-destructive"
+                            >
+                                <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+                                Logout
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                )
+                    : (<>
+                        <Link to="/login">
+                            <Button variant="ghost">Login</Button>
+                        </Link>
+                        <Link to="/signup">
+                            <Button variant="outline">Sign up</Button>
+                        </Link>
+                    </>
+                    )}
             </div>
         </nav>
     )
