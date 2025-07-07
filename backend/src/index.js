6b77c2623ef274b8209492b7701efb74765f0698
EgroegC <giorgosaleksakis@gmail.com>
2025-07-07 20:03:56 +0300

    refactor: dont connect to db if github runs tests. Remove detectOpenHandles flag.

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,10 +11,13 @@ app.use(cookieParser());
 require('./startup/cors')(app);
 require('./startup/logging')();
 require('./startup/routes')(app);
-require('./startup/db')();
 require('./config/jwt')();
 require('./startup/prod')(app);
 require('./startup/swagger')(app);
+
+if (process.env.IS_TEST !== 'true') {
+    require('./startup/db')();
+}
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => logger.info(`Listening on port ${port}...`));
--- backend/tests/globalTeardown.js
+++ backend/tests/globalTeardown.js
@@ -2,5 +2,7 @@ require("dotenv").config();
 const pool = require('../src/config/db')();
 
 module.exports = async () => {
-    await pool.end();
+    if (!pool.ended) {
+        await pool.end();
+    }
 };
